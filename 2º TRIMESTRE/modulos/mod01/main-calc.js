357c1c4d330ebdfdcc15720d3558b3a7567bd8e0
eduardagoncalvescarmo-max <eduarda.goncalves.carmo@escola.pr.gov.br>
2026-05-26 13:35:24 +0000

    .

---
+++ 2º TRIMESTRE/modulos/mod01/main-calc.js
@@ -0,0 +1,8 @@
+function multiplicarNumeros(n1, n2, n3) {
+    const resultado = n1* n2* n3;
+    return resultado;
+}
+const resultadoFinal = multiplicarNumeros(6, 8, 9);
+console.log(`O resultado da multiplicação dos três numeros é: ${resultadoFinal}`);
+const resultadoFinal2 = multiplicarNumeros(4, 2, 10);
+console.log(`O resultado da multiplicação dos três numeros é: ${resultadoFinal2}`);
