ac9dd2b31dbd3dc503e4741d11f01fe9f4e1d933
eduardagoncalvescarmo-max <eduarda.goncalves.carmo@escola.pr.gov.br>
2026-05-27 10:51:07 +0000

    finalização Módulo ESM - Petshop

---
+++ 2º TRIMESTRE/modulos/mod02-ESM/main-apelido-mod.js
@@ -0,0 +1,9 @@
+//USAMOS O * PARA DIZER QUE ESTAMOS
+//IMPORTANDO TUDO DE UM ARQUIVO 
+//O TERMO "AS" PERMITE CRIAR UM APELIDO 
+
+import * as apelido from './gato-mod.js'
+
+console.log(petShop.alimentarGato('Persa' , 'Petisco'))
+console.log(apelido.banhoGato('Siamês' , 'Completo'))
+console.log(apelido.tosaGato('Angorá' , 'Bem Baixinha'))
---
+++ 2º TRIMESTRE/modulos/mod02-ESM/main-gato-mod.js
@@ -0,0 +1,7 @@
+//APENAS IMPORTA MÓDULOS DE OUTROS ARQUIVOS (import)
+//OUTROS ARQUIVOS (import)
+import {alimentaGato, tosaGato, banhoGato} from './gato-mod.js'
+
+console.log(alimentaGato('Persa' , 'Petisco'))
+console.log(banhotaGato('Seamês' , 'Completo'))
+console.log(tosaGato('Angorá' , 'Bem Baixinha'))
